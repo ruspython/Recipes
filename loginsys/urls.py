87d5b52d3dcf46a7c92c7e71857b89f58937f720
vlad0058 <vlad0058@gmail.com>
2014-10-07 20:28:28 +0400

    Now author is a human :)

--- loginsys/urls.py
+++ loginsys/urls.py
@@ -5,5 +5,4 @@ urlpatterns = patterns('',
     url(r'^login/$', 'loginsys.views.login'),
     url(r'^logout/$', 'loginsys.views.logout'),
     url(r'^register/$', 'loginsys.views.register'),
-    url(r'^$', include('recipe.urls')),
 )
